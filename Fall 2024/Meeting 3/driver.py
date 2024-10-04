a90ed0bea98eda085edd357c7799d7aa2bdbf5d6
Ashton Cole <ashtoncole1028@gmail.com>
2024-10-04 16:56:17 -0500

    Introduced preliminary demo

--- Fall 2024/Meeting 3/driver.py
+++ Fall 2024/Meeting 3/driver.py
@@ -1,8 +1,9 @@
 import numpy as np
-import my_integrators
+import my_integrators as mi
 
 def main():
-    print('hello')
+    print('A very old differential equation')
+    f = lambda t, y: 1 - 3 * t + y + t ** 2 + t * y
 
 if __name__ == '__main__':
 	main()
--- Fall 2024/Meeting 3/my_integrators.py
+++ Fall 2024/Meeting 3/my_integrators.py
@@ -1,15 +1,36 @@
 import numpy as np
 
-def ode_integrator(method: str, f, tspan: tuple, y0: np.ndarray, dt: float):
+def ode_integrator(method: str, f, tspan: tuple, x0: np.ndarray, dt: float):
     """Integrate a system of ODE's using the forward Euler method.
     
     Arguments:
     f: a scalar- or vector-valued differential equation in the form f(t, x)
     tspan: a tuple of (t0, tf)
-    y0: the value at the initial time
+    x0: the value at the initial time
     dt: the time step
+    
+    Returns:
+    A tuple of time steps and corresponding solution vectors.
+    
+    Note: this is not necessarily recommended for very large systems.
     """
-    return 0
+    t = tspan[0]
+    x = x0
+    ts = [tspan[0]] # Might be prudent to pre-allocate these arrays
+    xs = [x0]
+    while t < tspan[1]:
+        if method == 'euler_step':
+            x = euler_step(f, x, t, dt)
+        else if method == 'rk2_step':
+            x = rk2_step(f, x, t, dt)
+        else if method == 'rk4_step':
+            x = rk4_step(f, x, t, dt)
+        else:
+            raise Exception(f'{method} is not defined within my_integrators!')
+        t += dt
+        ts += [t]
+        xs += [x]
+    return (ts, xs)
     
     
 def euler_step(f, xn, tn: float, dt: float):
@@ -17,10 +38,14 @@ def euler_step(f, xn, tn: float, dt: float):
     return xn + dt * f(tn, xn)
     
 def rk2_step(f, xn, tn: float, dt: float):
+    """Apply the Runge-Kutta 2 method for one time step."""
     # This is just the midpoint method!
-    return 0
+    k1 = f(tn, xn)
+    k2 = f(tn + dt / 2, xn + dt * k1 / 2)
+    return xn + dt * k2
 
 def rk4_step(f, xn, tn: float, dt: float):
+    """Apply the Runge-Kutta 4 method for one time step."""
     k1 = f(tn, xn)
     k2 = f(tn + dt / 2, xn + dt * k1 / 2)
     k3 = f(tn + dt / 2, xn + dt * k3 / 2)
